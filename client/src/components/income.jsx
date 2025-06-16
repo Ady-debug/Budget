6d5cdddec61116bf4c55a39c7bda2042b382d19d
Ady <adysoltan@gmail.com>
2025-06-16 19:49:57 +0100

    Remove unrequired boilerplate - #30

---
+++ client/src/components/income.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Income() {
+    return (
+        <div>
+            <h2>Income</h2>
+            <p>Wage: £2000</p>
+        </div>
+    )
+}
+
+export default Income
