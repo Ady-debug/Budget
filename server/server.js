4dc9782f5ebb8d31c4801f33efe880910a140b8f
Ady <adysoltan@gmail.com>
2025-07-02 17:13:42 +0100

    WIP: Setup boilerplate Fastify server TC: Setup API Layer #27

--- server/server.js
+++ server/server.js
@@ -1,6 +1,26 @@
 import "dotenv/config";
 import { createClient } from "@supabase/supabase-js";
+import Fastify from "fastify";
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async (request, reply) => {
+  return { hello: "World" };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
