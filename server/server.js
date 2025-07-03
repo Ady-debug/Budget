5791bf73c723203ce399126d1bfef105c8d264cb
Ady <adysoltan@gmail.com>
2025-07-03 17:19:14 +0100

    WIP: Implement GET and POST for income data - TC: Setup API Layer #27

--- server/server.js
+++ server/server.js
@@ -10,8 +10,34 @@ const fastify = Fastify({
   logger: true,
 });
 
-fastify.get("/", async (request, reply) => {
-  return { hello: "World" };
+fastify.get("/api/income", async (request, reply) => {
+  const { data, error } = await supabase
+    .from("income")
+    .select("income_type,amount");
+
+  if (error) {
+    reply.code(500).send({ error: error.message });
+    return;
+  }
+
+  return { income: data };
+});
+
+fastify.post("/api/income", async (request, reply) => {
+  console.log(request.body);
+
+  // UPDATE BELOW ONCE ROUTE TESTED IN API CLIENT
+  //   const { data, error } = await supabase
+  //     .from("income")
+  //     .insert([request.body])
+  //     .select();
+
+  //   if (error) {
+  //     reply.code(400).send({ error: error.message });
+  //     return;
+  //   }
+
+  //   return { income: data[0] };
 });
 
 fastify.post("/", async (request, reply) => {
@@ -30,7 +56,5 @@ const start = async () => {
 start();
 
 // TODO:
-// - Diagram the below for future understanding
-// - Create route to GET income info
-// - Create route to POST user info
 // - Create api.js for API layer
+// - Improve GET/POST ROUTES
