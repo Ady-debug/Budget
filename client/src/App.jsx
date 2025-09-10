8046c82041db9ab3bfcd355f4659b81475780d50
Ady <adysoltan@gmail.com>
2025-09-10 16:02:10 +0100

    Update background to add more gradient

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Income from "./components/Income";
 
 function App() {
   return (
-    <div className="bg-gradient-to-br from-blue-50 to-violet-200 dark:from-slate-700 dark:to-violet-900 min-h-screen">
+    <div className="bg-gradient-to-br from-blue-50 via-violet-200 to-teal-100 dark:from-slate-700 dark:via-indigo-900 dark:to-violet-900 min-h-screen">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center max-w-6xl mx-auto">
         <Income />
       </div>
