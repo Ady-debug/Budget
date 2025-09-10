769ab2440160ea4bd14925e5b4df0baea3445cf7
Ady <adysoltan@gmail.com>
2025-09-10 16:54:21 +0100

    WIP - Create template header - #18

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Income from "./components/Income";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="bg-gradient-to-br from-blue-50 via-violet-200 to-teal-100 dark:from-slate-700 dark:via-indigo-900 dark:to-violet-900 min-h-screen">
+      <Header />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center max-w-6xl mx-auto">
         <Income />
       </div>
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between px-4 py-2 bg-violet-100 dark:bg-indigo-800">
+      <div className="text-xl font-extrabold text-gray-900 dark:text-white">
+        &#128176; Budget
+      </div>
+      <div className="text-l text-gray-900 dark:text-white">Login &#8658;</div>
+    </header>
+  );
+}
