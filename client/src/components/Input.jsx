8c0e4767ed63284460a2a0d3ffa496ab77cfb58b
Ady <adysoltan@gmail.com>
2025-11-13 14:42:41 +0000

    BUG: Home and contents title display Fixes #41

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Input(props) {
   return (
     <label className="font-normal text-sm text-gray-700 dark:text-gray-400 flex gap-5 mb-1">
-      <span className="w-30">{props.title}:</span>
+      <span className="w-40">{props.title}:</span>
       <div className="before:content-['£']">
         <input
-          className="w-25 border rounded-lg border-transparent group-hover:border-indigo-500 transition-colors duration-100"
+          className="w-17 border rounded-lg border-transparent group-hover:border-indigo-500 transition-colors duration-100"
           name={props.name}
           type="number"
           inputMode="decimal"
--- client/src/components/Total.jsx
+++ client/src/components/Total.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Total(props) {
   return (
     <p className="font-normal text-gray-700 dark:text-gray-400 flex gap-5 mt-5">
-      <span className="w-30">{props.total > 0 && "Total:"}</span>
-      <span className="w-30">{props.total > 0 && `£${props.total}`}</span>
+      <span className="w-40">{props.total > 0 && "Total:"}</span>
+      <span className="w-17">{props.total > 0 && `£${props.total}`}</span>
     </p>
   );
 }
