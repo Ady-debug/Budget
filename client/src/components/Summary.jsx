a22d7b0f85b962ce5ea69fb1bc9f9c77c8f753f9
Ady <adysoltan@gmail.com>
2026-01-07 06:53:23 +0000

    Refactor Summary component to calculate total income and expenses dynamically

--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -5,10 +5,25 @@ export default function Summary(props) {
   const data = props.data;
   console.log(data);
 
-  // Create helper function to add all the values from category starting with income. This can be used against multiple categories for expenses
+  let totalIncome = 0;
+  let totalExpenses = 0;
 
-  const totalIncome = null;
-  const totalExpenses = null;
+  // Loops through each category in the budget, if that category is income then the value of each item is added to the totalIncome. If the category is not income, and therefore an expense, it is added to the totalExpenses.
+  for (let budgetCategory in data) {
+    for (let budgetItem in data[budgetCategory]) {
+      if (budgetCategory === "income") {
+        totalIncome += data[budgetCategory][budgetItem];
+      } else {
+        totalExpenses += data[budgetCategory][budgetItem];
+      }
+      console.log(`Budget category: ${budgetCategory}`);
+      console.log(`Budget item: ${budgetItem}`);
+      console.log(`Budget item amount: ${data[budgetCategory][budgetItem]}`);
+    }
+  }
+
+  // FIXME: Total is correct aside from a trailing .0000000000005 which needs investigating.
+
   const surplus = totalIncome - totalExpenses;
 
   return (
