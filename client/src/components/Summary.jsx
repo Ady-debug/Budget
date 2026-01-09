e8c421781beab854ba688054bee171ed6527a8c2
Ady <adysoltan@gmail.com>
2026-01-09 06:22:57 +0000

    Fix issue with floating point arithmetic in Summary component -US: Summary component Fixes #23

--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./card";
 
 export default function Summary(props) {
   const data = props.data;
-  console.log(data);
 
-  let totalIncome = 0;
-  let totalExpenses = 0;
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [totalExpenses, setTotalExpenses] = useState(0);
 
-  // Loops through each category in the budget, if that category is income then the value of each item is added to the totalIncome. If the category is not income, and therefore an expense, it is added to the totalExpenses.
-  for (let budgetCategory in data) {
-    for (let budgetItem in data[budgetCategory]) {
-      if (budgetCategory === "income") {
-        totalIncome += data[budgetCategory][budgetItem];
-      } else {
-        totalExpenses += data[budgetCategory][budgetItem];
+  function updateTotals(data) {
+    // Loops through each category in the budget, if that category is income then the value of each item is added to the totalIncome. If the category is not income, and therefore an expense, it is added to the totalExpenses.
+    let income = 0;
+    let expenses = 0;
+
+    for (let budgetCategory in data) {
+      for (let budgetItem in data[budgetCategory]) {
+        if (budgetCategory === "income") {
+          income += data[budgetCategory][budgetItem];
+        } else {
+          expenses += data[budgetCategory][budgetItem];
+        }
       }
-      console.log(`Budget category: ${budgetCategory}`);
-      console.log(`Budget item: ${budgetItem}`);
-      console.log(`Budget item amount: ${data[budgetCategory][budgetItem]}`);
+      setTotalIncome(parseFloat(income).toFixed(2));
+      setTotalExpenses(parseFloat(expenses).toFixed(2));
     }
   }
 
-  // FIXME: Total is correct aside from a trailing .0000000000005 which needs investigating.
+  const surplus = parseFloat(totalIncome - totalExpenses).toFixed(2);
 
-  const surplus = totalIncome - totalExpenses;
+  useEffect(() => {
+    updateTotals(data);
+  }),
+    [data];
+
+  // FIXME: Use effect not working as expected to update the totals dynamically so also needs investigating.
 
   return (
     <div>
